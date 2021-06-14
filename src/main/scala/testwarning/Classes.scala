27b3b26339e66cf8a0d52f96183904438a981b34
danarmak <danarmak@gmail.com>
2021-06-14 19:34:09 +0300

    Simpler testcase

--- src/main/scala/testwarning/Classes.scala
+++ src/main/scala/testwarning/Classes.scala
@@ -2,8 +2,6 @@ package testwarning
 
 sealed trait Trait1
 sealed trait Trait2
-sealed trait Trait3
 
 class Class1 extends Trait1
-class Class2 extends Trait2
-class Class3 extends Trait1 with Trait2
+// No class extends Trait1 with Trait2
--- src/main/scala/testwarning/Tester.scala
+++ src/main/scala/testwarning/Tester.scala
@@ -3,7 +3,7 @@ package testwarning
 object Tester {
   def test(x: Trait1): String = x match {
     case y: Trait2 => "Also Trait2"
-    case z: Trait3 => "Also Trait3"
     case _ => "Only Trait1"
   }
 }
+
